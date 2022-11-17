3a87a96d9a65f0e4b4f1acc4c32fc464e7c13e7b
Vitaliy Skoryy <102066708+Skyvi0@users.noreply.github.com>
2022-11-17 14:08:32 +0100

    Finished

--- src/main/java/j/kurs/App.java
+++ src/main/java/j/kurs/App.java
@@ -2,8 +2,36 @@ package j.kurs;
 
 public class App {
     public static void main(String[] args) {
+        Inventory inventory = new Inventory(10);
+        Tisch tisch = new Tisch(10, true);
+        inventory.add(tisch);
+        inventory.add(new Tisch(20, false));
+        inventory.add(new Tisch(30, true));
+
+        // remove item from the inventory
+        inventory.remove(tisch);
+
+        // get the total weight of the inventory
+
         Transportierbar glasTisch = new Tisch(30, true);
         System.out.println("Das Gewicht des Tisches ist: " + glasTisch.gewicht() + "kg" + " und zerbrechlich: "
                 + glasTisch.zerbrechlich());
+                System.out.println("Total weight: " + inventory.totalWeight());
     }
+    // add an item to the inventory
+    public static void add(Inventory inventory, Transportierbar item) {
+        inventory.add(item);
+
+    }
+    // remove an item from the inventory
+    public static void remove(Inventory inventory, Transportierbar item) {
+        inventory.remove(item);
+
+    }
+// get the total weight of the inventory
+public static float totalWeight(Inventory inventory) {
+    return inventory.totalWeight();
 }
+
+}
+
---
+++ src/main/java/j/kurs/Inventory.java
@@ -0,0 +1,41 @@
+package j.kurs;
+public class Inventory {
+    // array of Transportierbar objects (Tisch, Stuhl, etc.)
+    private final Transportierbar[] inventar;
+    // number of items in the inventory
+    private int count;
+    // maximum number of items in the inventory 
+    private final int max;
+    // constructor with max number of items in the inventory as parameter 
+    public Inventory(int max) {
+        this.max = max;
+        inventar= new Transportierbar[max];
+        count = 0;
+    }
+    // add an item to the inventory 
+    public void add(Transportierbar item) {
+        if (count < max) {
+            inventar[count] = item;
+            count++;
+        }
+    }
+    // remove an item from the inventory 
+    public void remove(Transportierbar item) {
+        for (int i = 0; i < count; i++) {
+            if (inventar[i] == item) {
+                inventar[i] = inventar[count - 1];
+                inventar[count - 1] = null;
+                count--;
+                break;
+            }
+        }
+    }
+    // get the total weight of the inventory 
+    public float totalWeight() {
+        float total = 0;
+        for (int i = 0; i < count; i++) {
+            total += inventar[i].gewicht();
+        }
+        return total;
+    }
+}
